ea7fd1b3c00814a98840dbfd1d3d5af7809018a9
Jorgel <jorgeluizandrade@outlook.com>
2015-11-18 22:14:27 -0200

    Initial commit. Working game

---
+++ JogoDaVelha/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class Game
+    {
+        private char[,] board;
+        private char currentPlayer;
+        private bool hasWinner;
+        private char winner;
+
+        public Game()
+        {
+            board = new char[,] {   { ' ', ' ', ' '},
+                                    { ' ', ' ', ' '},
+                                    { ' ', ' ', ' '} };
+
+            currentPlayer = 'X';
+            hasWinner = false;
+        }
+
+        public char markCell(int index)
+        {
+            int x = index % 3;
+            int y = index / 3;
+
+            board[x, y] = currentPlayer;
+
+            if (currentPlayer == 'X')
+                currentPlayer = 'O';
+            else
+                currentPlayer = 'X';
+
+            return checkBoard();
+        }
+
+        public char getCurrentPlayer()
+        {
+            return currentPlayer;
+        }
+
+        public char checkBoard()
+        {
+            char winner = ' ';
+
+            // Check if some column has a winner
+            for (int i = 0; i < 3; i++)
+            {
+                if(board[i, 0] != ' ' 
+                    && board[i, 0] == board[i,1]
+                    && board[i, 0] == board[i, 2])
+                {
+                    winner = board[i, 0];
+                    break;
+                }
+            }
+
+            if (winner != ' ')
+                return winner;
+
+            // Check if some line has a winner
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] != ' '
+                    && board[0, i] == board[1, i]
+                    && board[0, i] == board[2, i])
+                {
+                    winner = board[0, i];
+                    break;
+                }
+            }
+
+            if (winner != ' ')
+                return winner;
+
+            //Check diagonals
+            if( board[0, 0] != ' '
+                && board[0,0] == board[1,1] 
+                && board[0,0] == board[2,2])
+            {
+                return board[0, 0];
+            }
+
+            if (board[0, 2] != ' '
+                && board[0, 2] == board[1, 1]
+                && board[0, 2] == board[2, 0])
+            {
+                return board[0, 0];
+            }
+
+            return ' ';
+        }
+    }
+}
---
+++ JogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JogoDaVelha
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private bool hasWinner = false;
+        Game game = new Game();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            txtPlayer.Text = "X";
+        }
+
+        private void TextBlock_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void Cell_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (hasWinner)
+                return;
+
+            var cell = (Grid)sender;
+
+            var name = cell.Name;
+            int index = name[name.Length - 1] - '0';
+
+            var txt = cell.Children.OfType<TextBlock>().FirstOrDefault();
+            txt.Text = game.getCurrentPlayer().ToString();
+            var winner = game.markCell(index);
+            
+            if(winner != ' ') //Has winner
+            {
+                txtVencedor.Text = "O vencedor é o jogador " + winner;
+                hasWinner = true;
+            }
+            else
+            {
+                txtPlayer.Text = game.getCurrentPlayer().ToString();
+            }
+
+
+        }
+    }
+}
